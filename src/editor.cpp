3c22a6ae28829323435875db83d2cd5f24455c6d
tomascabrerizo <tomascabrerizo97@gmail.com>
2023-12-11 19:10:26 -0300

    [week 11] only project gizmo when is infront of the camera

--- src/editor.cpp
+++ src/editor.cpp
@@ -418,32 +418,38 @@ void Editor::RenderEditorGizmos() {
    
     Vec3 v = transform.pos - camera.pos;
 
-    f32 t = 0;
-    if(ABS(v.Dot(n)) > 0.01f)  {
-        t = (o.Dot(n) - p.Dot(n)) / v.Dot(n);
+    f32 behindTest = v.Dot(n);
+
+    if(behindTest >= 0) {
+
+        f32 t = 0;
+        if(ABS(v.Dot(n)) > 0.01f)  {
+            t = (o.Dot(n) - p.Dot(n)) / v.Dot(n);
+        }
+
+        transform.rot = Vec3(0,0,0);
+        transform.scale = Vec3(1,1,1);
+        transform.pos = p + v * t; 
+
+        GraphicsManager::Get()->SetWorldMatrix(transform.GetWorldMatrix());
+        ModelManager::Get()->SetTexture(transformGizmoZ, "red.png");
+        RenderModel(transformGizmoX, Vec3(1,0,0));
+        
+        TransformCMP transform1 = transform;
+        transform1.rot.z += (f32)TO_RAD(90);
+
+        GraphicsManager::Get()->SetWorldMatrix(transform1.GetWorldMatrix());
+        ModelManager::Get()->SetTexture(transformGizmoZ, "green.png");
+        RenderModel(transformGizmoY, Vec3(0,1,0));
+
+        TransformCMP transform2 = transform;
+        transform2.rot.y += (f32)TO_RAD(90);
+        
+        GraphicsManager::Get()->SetWorldMatrix(transform2.GetWorldMatrix());
+        ModelManager::Get()->SetTexture(transformGizmoZ, "blue.png");
+        RenderModel(transformGizmoZ, Vec3(0,0,1));
+
     }
-    
-    transform.rot = Vec3(0,0,0);
-    transform.scale = Vec3(1,1,1);
-    transform.pos = p + v * t; 
-
-    GraphicsManager::Get()->SetWorldMatrix(transform.GetWorldMatrix());
-    ModelManager::Get()->SetTexture(transformGizmoZ, "red.png");
-    RenderModel(transformGizmoX, Vec3(1,0,0));
-    
-    TransformCMP transform1 = transform;
-    transform1.rot.z += (f32)TO_RAD(90);
-
-    GraphicsManager::Get()->SetWorldMatrix(transform1.GetWorldMatrix());
-    ModelManager::Get()->SetTexture(transformGizmoZ, "green.png");
-    RenderModel(transformGizmoY, Vec3(0,1,0));
-
-    TransformCMP transform2 = transform;
-    transform2.rot.y += (f32)TO_RAD(90);
-    
-    GraphicsManager::Get()->SetWorldMatrix(transform2.GetWorldMatrix());
-    ModelManager::Get()->SetTexture(transformGizmoZ, "blue.png");
-    RenderModel(transformGizmoZ, Vec3(0,0,1));
 
     GraphicsManager::Get()->SetDepthStencilState(true);
 }
